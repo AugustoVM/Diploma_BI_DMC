af13e3955a7d0fa8f136249c4aa9ed0491ed0498
AugustoVM <avillarrealmeza@gmail.com>
2026-06-01 20:24:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ st.title("Proyecto final Diploma BI")
 #incluir titulo en una barra lateral
 st.sidebar.title("Parámetros")
 
-st.image("logophyton.png",width=500)
-st.sidebar.image("logoDMC.png",width=300)
+st.image("logophyton.png",width=250)
+st.sidebar.image("logoDMC.png",width=150)
