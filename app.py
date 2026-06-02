55ec18c3f07b37a134c9165281dcc98c7ba7c853
AugustoVM <avillarrealmeza@gmail.com>
2026-06-01 20:20:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+#Incluir titutlo
 st.title("Proyecto final Diploma BI")
-
+#incluir titulo en una barra lateral
 st.sidebar.title("Parámetros")
+
+st.image("logophyton.png")
+st.sidebar.image("logoDMC.png")
