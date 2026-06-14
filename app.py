e15c8e233a553422ed19f7334963b26df14497e5
AugustoVM <avillarrealmeza@gmail.com>
2026-06-14 17:28:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,20 +12,23 @@ st.write("Elaborado por: Cesar Augusto Villarreal")
 
 modulos=st.sidebar.selectbox("Seleccione un modulo",["Home","Carga y perfil del dataset","Procesamiento de datos","Analisis visual"]) #es un modulo que me permitira mostrra un dspliegue de distitas eclecciones a traves de una lista
 
-
-archivo= st.file_uploader("Cargue el archivo excel o csv")
-
-if archivo is not None:
-  
-  if archivo.name.endswith(".csv"):
-     data = pd.read_csv(archivo)
-     st.write(data)
-  elif archivo.name.endswith(".xlsx"):
-      data = pd.read_excel(archivo)
-      st.write(data)
-  else: 
-       st.write("Formato no valido")
-
-else:
-    st.write("Por favor cargue su archivo") 
+       if modulos == "Home":
+         st.write("Bienvenido a la aplicación:")
+       elif modulos == "Carga y perfil del dataset":
+         
+            archivo= st.file_uploader("Cargue el archivo excel o csv")
+            
+            if archivo is not None:
+              
+              if archivo.name.endswith(".csv"):
+                 data = pd.read_csv(archivo)
+                 st.write(data)
+              elif archivo.name.endswith(".xlsx"):
+                  data = pd.read_excel(archivo)
+                  st.write(data)
+              else: 
+                   st.write("Formato no valido")
+            
+            else:
+                st.write("Por favor cargue su archivo") 
 
