ff5771afa15b8f66fb3883714c044fa1ae0bd996
AugustoVM <avillarrealmeza@gmail.com>
2026-06-13 21:00:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,5 @@ st.image("logophyton.png",width=250)
 st.sidebar.image("logoDMC.png",width=120)
 
 st.write("Elaborado por: Cesar Augusto Villarreal")
+
+archivo= st.file_uploader("Cargue el archivo excel o csv")
