5dc368b4092ecc015aa8e3830b246005b5a2ab06
AugustoVM <avillarrealmeza@gmail.com>
2026-06-13 19:46:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,12 @@ st.write("Elaborado por: Cesar Augusto Villarreal")
 
 archivo=st.file_uploader("Cargue el archivo CSV o Excel")
 
+if archivo is not None :
+  if archivo.name.endswith(".CSV"):
+        data=pd.read_csv(archivo)
+        st.write(data)
+  elif archivo.name.endswith(".xlsx"):
+        data=pd.read_excel(archivo)
+        st.writw(data)
+  else :
+  st.write("formato no valido")
