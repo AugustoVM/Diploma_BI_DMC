3af4f1d901e91653bfc6b576f5920f74786813eb
AugustoVM <avillarrealmeza@gmail.com>
2026-06-13 21:15:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,18 @@ st.sidebar.image("logoDMC.png",width=120)
 st.write("Elaborado por: Cesar Augusto Villarreal")
 
 archivo= st.file_uploader("Cargue el archivo excel o csv")
+
+if archivo is not None:
+  
+  if archivo.name.endswith(".csv"):
+     data = pd.read_csv(archivo)
+     st.write(data)
+  elif archivo.name.endswith(".xlsx"):
+    data = pd.read_excel(archivo)
+    st.write(data)
+  else: 
+    st.write("Formato no valido")
+
+  else :
+    st.write("Por favor cargue su archivo") 
+
